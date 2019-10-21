ca15bcf27b1ddd097aa6b2fa24a33eaef85598bb
Daniel Ermilson Velásquez Ocampo <dvelas26@gmail.com>
2019-10-21 13:53:41 -0500

    Using props and state to dinamycally output content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+      persons: [
+          {name: 'Max', age: 28},
+          {name: 'Manu', age: 29},
+          {name: 'Stephanie', age: 26}
+      ]
+  }
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a react app</h1>
-           <Person name="Max" age="28"/>
-           <Person name="Manu" age="29"> My Hobbies: Racing </Person>
-           <Person name="Stephanie" age="26"/>
+           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
+           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, I\'m a react app'));
