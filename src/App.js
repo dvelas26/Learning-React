fab51c13ad3182405748c4a0509ee8b9210c0a3a
Daniel Ermilson Velásquez Ocampo <dvelas26@gmail.com>
2019-10-21 14:08:20 -0500

    Switching name and age using setState method

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ class App extends Component {
           {name: 'Max', age: 28},
           {name: 'Manu', age: 29},
           {name: 'Stephanie', age: 26}
-      ]
+      ],
+      otherState: 'some other value'
   }
+
+  switchNameHandler = () =>  {
+    this.setState({persons: [
+      {name: 'Maximilian', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 27}
+    ]})
+  }
+
   render() {
     return (
       <div className="App">
-          <h1>Hi, I'm a react app</h1>
-           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
-           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <h1>Hi, I'm a react app</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My Hobbies: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, I\'m a react app'));
